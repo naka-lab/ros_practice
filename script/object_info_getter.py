db48fbfdc9606ebe68278ffcafc6866c6c266dc8
naka_t <20672947+naka-tomo@users.noreply.github.com>
2022-04-26 10:28:06 +0900

    Update object_info_getter.py

--- script/object_info_getter.py
+++ script/object_info_getter.py
@@ -12,16 +12,11 @@ def callback(data):
         print( "lefttop:", o["lefttop"] )
         print( "rightbottom:", o["rightbottom"] )
         print( "position:", o["position"] )
-        #print( "position_mindepth:", o["position_mindepth"] )
-        #print( "position_center:", o["position_center"] )
-        #print( "position_bottom:", o["position_bottom"] )
         print( "-----------" )
     
 def main():
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("/object_rec/object_info", String, callback)
-    #rospy.Subscriber("/ssd_object_rec/object_info", String, callback)
-    #rospy.Subscriber("/ar_marker_rec/object_info", String, callback)
+    rospy.Subscriber("/ar_marker_rec/object_info", String, callback)
     rospy.spin()
         
 if __name__ == '__main__':
